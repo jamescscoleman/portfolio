9494aafb8a951692f210c256f5cdcc161ed3793a
James Coleman <157778141+jamescscoleman@users.noreply.github.com>
2026-08-08 00:42:59 -0700

    Remove added PhoneBelt website link

--- src/data/projects.js
+++ src/data/projects.js
@@ -43,12 +43,6 @@ const sourceProjects = [
       'Non-dilutive funding secured',
     ],
     stages: ['POC', 'MVP', 'Prototype', 'Final Product'],
-    links: [
-      {
-        label: 'Visit the PhoneBelt website',
-        href: 'https://phonebelt-site.jamescscoleman.chatgpt.site',
-      },
-    ],
     sections: [
       {
         heading: 'Why commercial driving is a different problem',
